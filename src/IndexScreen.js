615bd15fbe11d26bfccb8ac4cdb2d856363f70a7
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-17 19:45:36 -0800

    set up

---
+++ src/IndexScreen.js
@@ -0,0 +1,20 @@
+//Goal of this component is to show a list of blog posts
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity
+} from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default IndexScreen;
