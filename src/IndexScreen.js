9f0d5c529943ee9719bf83cd7224c920d4c19110
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-24 18:32:51 -0800

    Update IndexScreen.js

--- src/IndexScreen.js
+++ src/IndexScreen.js
@@ -9,14 +9,13 @@ import {
 } from "react-native";
 import { Context } from "./context/BlogContext";
 import React, { useContext } from "react";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context); //value is the value that was passed to the BlogProvider component in App.js
 
   return (
     <View>
-      <Text>Index Screen</Text>
-
       <Button title="Add Post" onPress={addBlogPost} />
 
       <FlatList
@@ -24,9 +23,10 @@ const IndexScreen = () => {
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity>
-              <Text>{item.title}</Text>
-            </TouchableOpacity>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash" />
+            </View>
           );
         }}
       />
@@ -34,5 +34,20 @@ const IndexScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray"
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 export default IndexScreen;
