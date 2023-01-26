ba63ad51bd573779f946d5e78edf5937da859842
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-25 17:26:09 -0800

    Update CreateScreen.js

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { Context } from "../context/BlogContext";
 
 const CreateScreen = ({ navigation }) => {
@@ -8,13 +8,41 @@ const CreateScreen = ({ navigation }) => {
   const blogPost = state.find((blogPost) => blogPost.id === id);
   console.log(id, state);
 
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <View>
-      <Text>Create</Text>
+      <Text style={styles.label}>Enter Title</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChange={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChange={(text) => setContent(text)}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5
+  }
+});
 
 export default CreateScreen;
