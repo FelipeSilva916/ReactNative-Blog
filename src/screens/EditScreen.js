af4829af7b81f81981e46a382e5388aa0282b9b3
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-25 19:34:26 -0800

    Update EditScreen.js

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Context } from "../context/BlogContext";
+import { TextInput } from "react-native-gesture-handler";
 
 const EditScreen = ({ navigation }) => {
-  const id = navigation.getParam("id");
+  const postId = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
+
+  const blogPost = state.find((blogPost) => blogPost.id === postId);
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>Edit Screen - {id} </Text>
+      <Text>Edit Title: </Text>
+      <TextInput value={title} onChangeText={(text) => setTitle(text)} />
+      <Text>Edit Content: </Text>
+      <TextInput value={content} onChangeText={(text) => setContent(text)} />
     </View>
   );
 };
