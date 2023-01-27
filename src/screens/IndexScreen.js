b125dfa10224e3192ea72007f092beaae4947131
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-27 04:38:33 -0800

    add blog post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -16,6 +16,13 @@ const IndexScreen = ({ navigation }) => {
 
   useEffect(() => {
     getBlogPosts();
+
+    const listener = navigation.addListener("didFocus", () => {
+      getBlogPosts();
+    });
+    return () => {
+      listener.remove();
+    };
   }, []);
 
   return (
